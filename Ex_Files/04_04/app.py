a2862cd6a36441c11a72f1425d44b9ac4e2c8c66
Syarwina Ridwan <114587158+syarwinaaa09@users.noreply.github.com>
2026-04-16 04:25:43 +0000

    Enhance search functionality to include first names in laureate list

--- Ex_Files/04_04/app.py
+++ Ex_Files/04_04/app.py
@@ -26,9 +26,10 @@ def laureate_list():
 
     # tip: remember that laureate["name"] contains a first name
     for laureate in laureates:
+        firstname = laureate["firstname"].lower()
         surname = laureate["surname"].lower()
         # your code here
-        if search_string in surname:
+        if search_string in firstname or search_string in surname:
             results.append(laureate)
 
     return jsonify(results)
